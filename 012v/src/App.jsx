8197a788d110abc9bdc750af6518645496bafb20
Soumo525 <soumodhali16@gmail.com>
2023-12-28 19:45:59 +0530

    react library and JSX

--- 012v/src/App.jsx
+++ 012v/src/App.jsx
@@ -2,10 +2,14 @@
 import Chai from "./chai"
 
 function App() {
-  
+  const username = "How to Die"
 
   return (
-   <Chai />
+    <>
+      <Chai />
+      {username}
+    </>
+   
   )
 }
 
--- 012v/src/main.jsx
+++ 012v/src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return(
+    <div>
+      <h1>
+        Custom App
+      </h1>
+    </div>
+  )
+}
+const anotherUser = "Chai aur React"
+
+const anotherElement = (
+  <a href = "https://google.com" target = '_blank'>Visit Google</a>
+)
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me ',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  reactElement
+  
 )
